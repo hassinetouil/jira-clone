91a5ba976d30a1d771fc171517452416d0244138
Hassine TOUIL <hassine.touil@defensy.io>
2024-11-20 23:27:18 +0100

    adding component libary

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="lg">Click me</Button>
+      <p className="text-red-500 font-semibold">
+        Hassine
+      </p>
+    </div>
+  );
+}
